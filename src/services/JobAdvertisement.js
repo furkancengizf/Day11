8d6337b5dcffc2e7ff4a16ca55d15dbfbaf82526
furkancengizf <78604026+furkancengizf@users.noreply.github.com>
2021-08-24 12:11:07 +0300

    Update JobAdvertisement.js

--- src/services/JobAdvertisement.js
+++ src/services/JobAdvertisement.js
@@ -2,7 +2,7 @@ import axios from "axios";
 
 export default class JobAdvertisementService{
    getJobs(){
-     return axios.get{"http://localhost:8080/api/jopadvertisement/getall"}
+     return axios.get{"http://localhost:8080/api/jobadvertisement/getall"}
    
    }
 
